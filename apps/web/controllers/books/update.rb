c32eb8ec7e68cf58d566a37a8b8f906a54325de8
Dmitry Stepanenko <sdm102@yandex.ua>
2016-07-13 13:03:40 +0300

    Update book functionality added.

--- apps/web/controllers/books/update.rb
+++ apps/web/controllers/books/update.rb
@@ -2,7 +2,17 @@ module Web::Controllers::Books
   class Update
     include Web::Action
 
-    def call(params); end
+    expose :book
     params BookParams
+
+    def call(params)
+      @book = BookRepository.find(params[:id])
+      @book.update(params[:book])
+
+      if params.valid?
+        BookRepository.update(@book)
+        redirect_to routes.books_path
+      end
+    end
   end
 end
