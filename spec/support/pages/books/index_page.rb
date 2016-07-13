808e31c1dd4ad1013c1f2d31fce6bcc0cf1effd7
Dmitry Stepanenko <sdm102@yandex.ua>
2016-07-13 13:35:07 +0300

    Update index page object to consider books table.

--- spec/support/pages/books/index_page.rb
+++ spec/support/pages/books/index_page.rb
@@ -4,6 +4,13 @@ module Books
   class IndexPage < ::GeneralPage
     set_url '/books'
 
-    elements :books, '#books .book'
+    class BookRow < SitePrism::Section
+      element :title, 'td:nth-child(1)'
+      element :author, 'td:nth-child(2)'
+      element :edit, 'td:nth-child(3) a:first-child'
+      element :delete, 'td:nth-child(3) a:last-child'
+    end
+
+    sections :books, BookRow, '#books table tr'
   end
 end
